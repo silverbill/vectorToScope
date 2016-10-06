ba753d3b78d22552c04b76a7b19cf0e13d6bd664
Bryan Sory <bryansory@icloud.com>
2016-10-06 05:28:34 -0500

    2

---
+++ ConsoleApplication1/challenge2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+    public class Addition
+    {
+        static Func<int, int, int> sum = (a, b) => (a + b);
+        public static void Main()
+        {
+            Console.WriteLine(sum(1, 2) == 3);
+            Console.ReadLine();
+        }
+    }
+
+
+
