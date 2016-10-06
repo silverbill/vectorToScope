48435f59a5e776debf5043859f5171bd1932985c
Bryan Sory <bryansory@icloud.com>
2016-10-06 05:44:44 -0500

    3

---
+++ challenge3/challenge3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+public class randomnumber
+{
+    static int random(int min, int max)
+    {
+        Random r = new Random();
+
+        double randomDouble = r.NextDouble() * (max - min) + min;
+
+        return (int)randomDouble;
+
+    }
+    public static void Main()
+    {
+        Console.WriteLine(random(1, 2) <= 2 && random(1, 2) >= 1);
+        Console.WriteLine(random(-11, 22) <= 22 && random(-11, 22) >= -11);
+        Console.ReadLine();
+    }
+}
